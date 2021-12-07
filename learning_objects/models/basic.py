2aa015ea00eae94f172b4f088d4339af07e90f90
Rajat Talak <talak@mit.edu>
2021-12-07 17:42:04 -0500

    Adding pose_shape.py that implements PACE

--- learning_objects/models/basic.py
+++ learning_objects/models/basic.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
-import  numpy as np
-import torch.nn.functional as f
+# import numpy as np
+# import torch.nn.functional as f
 from torch.autograd import Function
 from point_transformer import PointTransformerSegment
 from pointnet import PointNetDenseCls
 
+import autograd.numpy as np
 
 
 
@@ -74,11 +75,8 @@ class Keypoints(nn.Module):
         """
         Inputs: 
         ------
-            k: int32
-               number of keypoints 
-            method: string. Takes values:
-                'point_transformer'
-                'pointnet'
+            k       : int32   : number of keypoints 
+            method  : string  : takes values: {'point_transformer', 'pointnet'}
         """
         self.k = k
         self.method = method
@@ -136,33 +134,142 @@ class Keypoints(nn.Module):
 
         return y
 
-class Pace(Function):
-
-    @staticmethod
-    def forward(ctx, y, b):
-        # y = keypoints of the input point cloud
-        # b = keypoints of cad models
-
-        # output: rotation, translation, and shape parameter
-
-        raise NotImplementedError
-
-
-    @staticmethod
-    def backward(ctx):
-        # this should implement gradient of output (rotation, translation, and shape) wrt input y (b are fixed)
-
-        raise NotImplementedError
 
 
 
-class GenerateModel(Function):
+class Rotation(nn.Module):
+    def __init__(self, Q):
+        super(Rotation, self).__init__()
+        """
+        Q: 10x10 torch.tensor
+        """
+        self.n = Q.shape[0]
+        A = []
+        for i in range(16):
+            A.append(np.zeros((n, n)))
 
-    @staticmethod
-    def forward(ctx, R, t, c):
-        # R = rotation, t = translation, c = shape
-        # output = model point cloud or cad model
-        raise NotImplementedError
+        A[0][0, 0] = 1
+
+        A[1][0, 0] = 1
+        A[1][1, 1] = -1
+        A[1][2, 2] = -1
+        A[1][3, 3] = -1
+
+        A[2][0, 0] = 1
+        A[2][4, 4] = -1
+        A[2][5, 5] = -1
+        A[2][6, 6] = -1
+
+        A[3][0, 0] = 1
+        A[3][7, 7] = -1
+        A[3][8, 8] = -1
+        A[3][9, 9] = -1
+
+        A[4][1, 4] = 1
+        A[4][2, 5] = 1
+        A[4][3, 6] = 1
+        A[4][4, 1] = 1
+        A[4][5, 2] = 1
+        A[4][6, 3] = 1
+
+        A[5][3, 4] = 1
+        A[5][1, 6] = 1
+        A[5][3, 9] = 1
+        A[5][4, 3] = 1
+        A[5][6, 1] = 1
+        A[5][9, 3] = 1
+
+        A[6][4, 7] = 1
+        A[6][5, 8] = 1
+        A[6][6, 9] = 1
+        A[6][7, 4] = 1
+        A[6][8, 5] = 1
+        A[6][9, 6] = 1
+
+        A[7][2, 6] = 1
+        A[7][3, 5] = -1
+        A[7][0, 7] = -1
+        A[7][6, 2] = 1
+        A[7][5, 3] = -1
+        A[7][7, 0] = -1
+
+        A[8][3, 4] = 1
+        A[8][1, 6] = -1
+        A[8][0, 8] = -1
+        A[8][4, 3] = 1
+        A[8][6, 1] = -1
+        A[8][8, 0] = -1
+
+        A[9][1, 5] = 1
+        A[9][0, 9] = -1
+        A[9][2, 4] = -1
+        A[9][5, 1] = 1
+        A[9][9, 0] = -1
+        A[9][4, 2] = -1
+
+        A[10][5, 9] = 1
+        A[10][0, 1] = -1
+        A[10][6, 8] = -1
+        A[10][9, 5] = 1
+        A[10][1, 0] = -1
+        A[10][8, 6] = -1
+
+        A[11][6, 7] = 1
+        A[11][4, 9] = -1
+        A[11][0, 2] = -1
+        A[11][7, 6] = 1
+        A[11][9, 4] = -1
+        A[11][2, 0] = -1
+
+        A[12][4, 8] = 1
+        A[12][0, 3] = -1
+        A[12][5, 7] = -1
+        A[12][8, 4] = 1
+        A[12][3, 0] = -1
+        A[12][7, 5] = -1
+
+        A[13][3, 8] = 1
+        A[13][2, 9] = -1
+        A[13][0, 4] = -1
+        A[13][8, 3] = 1
+        A[13][9, 2] = -1
+        A[13][4, 0] = -1
+
+        A[14][1, 9] = 1
+        A[14][0, 5] = -1
+        A[14][3, 7] = -1
+        A[14][9, 1] = 1
+        A[14][5, 0] = -1
+        A[14][7, 3] = -1
+
+        A[15][2, 7] = 1
+        A[15][1, 8] = -1
+        A[15][0, 6] = -1
+        A[15][7, 2] = 1
+        A[15][8, 1] = -1
+        A[15][6, 0] = -1
+
+        b = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        X = cp.Variable((n, n), symmetric=True)
+        constraints = [X >> 0]
+        constraints += [
+            cp.trace(A[i] @ X) == b[i] for i in range(16)
+        ]
+
+        prob = cp.Problem(cp.Minimize(cp.trace(Q @ X)), constraints=constraints)
+        prob.solve()
+
+        # Step(2): projects X.vale onto the rank 1 PSD manifold and extracts the rotation matrix R
+        u, s, vh = np.linalg.svd(X.value)
+        u0 = np.sqrt(s[0]) * u[:, 0]
+        u0 = u0 / u0[0]
+        rvec = u0[1:]
+        R = rvec.reshape((3, 3))
+
+
+
+
 
 
 
@@ -210,4 +317,8 @@ if __name__ == '__main__':
     y = kp(pointcloud=pc)
     print(y.size())
     print('-' * 20)
+
+
+    # Test: PACE
+    
 
