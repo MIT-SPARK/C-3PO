7c567796b37cf1f2bd2febcb67d84105e59d0a96
Rajat Talak <talak@mit.edu>
2022-01-08 12:10:03 -0500

    adding code for certifiability metric

--- learning_objects/experiments/expt_keypoint_corrector_analysis/expt_keypoint_corrector.py
+++ learning_objects/experiments/expt_keypoint_corrector_analysis/expt_keypoint_corrector.py
@@ -5,19 +5,5 @@ This code is to experiment with the keypoint_corrector.
 2. What is the best way to optimize the keypoint correction? What algorithm works and what doesn't?
 3. What is the best cost function to use to achieve our end-objective: chamfer loss, soft chamfer loss, a combination
     of chamfer and keypoint loss?
+
 """
-
-
-# Get an input point cloud model with sufficient number of keypoints
-
-
-
-# Generate inputs by transforming and occluding the input point cloud. Add abberations to the keypoints.
-
-
-
-# See if the PACE + Keyoint Correction Module is able to detect corrections.
-
-
-
-
