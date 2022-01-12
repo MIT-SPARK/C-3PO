e4400a1cad6e1615281855a5ae8abaf85f1368d8
lisapeng <lisapeng@mit.edu>
2022-01-12 11:02:55 -0500

    apolloscape utils, tsdf avg code in modelgen, docs

--- learning_objects/models/modelgen.py
+++ learning_objects/models/modelgen.py
@@ -7,6 +7,7 @@ Note:
 
 import torch
 import torch.nn as nn
+import open3d as o3d
 
 import os
 import sys
@@ -33,6 +34,62 @@ class ModelFromShape():
         self.cad_models = cad_models.unsqueeze(0)  # (1, K, 3, n)
         self.model_keypoints = model_keypoints.unsqueeze(0)  # (1, K, 3, N)
 
+
+    def tsdf_to_mesh(self, avg_tsdf, visualization=False):
+        """
+        Use marching cubes to generate meshes from a list of TSDF volumes
+        """
+        vertices, triangles = libmcubes.marching_cubes(-avg_tsdf, 0)
+        vertices /= self.options.resolution
+        vertices -= 0.5
+        avg_mesh = {"vertices": vertices, "triangles": triangles}
+
+        if visualization:
+            o3d_mesh = o3d.geometry.TriangleMesh()
+            o3d_mesh.vertices = o3d.utility.Vector3dVector(avg_mesh['vertices'])
+            o3d_mesh.triangles = o3d.utility.Vector3iVector(avg_mesh['triangles'])
+            o3d_mesh.compute_vertex_normals()
+            o3d_mesh.compute_triangle_normals()
+            o3d.visualization.draw_geometries([o3d_mesh])
+        return avg_mesh
+
+
+    def average_tsdf(self, weights):
+        """
+
+        :param weights: list of numbers that sum up to 1 that represent the weighting of cad model average
+        :return:
+        """
+        #returns an averaged tsdf
+        model_paths = []
+        for i in range(len(self.cad_models.shape[0])):
+            model_name = ""  # cad_models.name
+            model_paths.append(os.path.join(tsdf_dir, model_name + ".npy"))
+        tsdf_list = self.load_tsdf(model_paths)
+
+        assert (len(tsdf_list) == len(weights))
+        avg_tsdf = np.zeros(tsdf_list[0].shape)
+        weights_sum = 0
+        for tsdf, weight in zip(tsdf_list, weights):
+            avg_tsdf += tsdf * weight
+            weights_sum += weight
+        avg_tsdf /= weights_sum
+        return avg_tsdf
+
+
+    def load_tsdf(self, model_paths):
+        """
+
+        :param model_paths: list of model names e.g. [<path_to>/aodi-Q7-SUV.npy, <path_to>/aodi-a6.off.npy]
+        :return: tsdf_list: list of tsdf models
+        """
+        tsdf_list = []
+        for filepath in model_paths:
+            tsdf = np.load(filepath)
+            tsdf_list.append(tsdf)
+
+        return tsdf_list
+
     def forward(self, shape):
         """
         shape: torch.tensor of shape (B, K, 1)
@@ -49,7 +106,7 @@ class ModelFromShape():
         model: torch.tensor of shape (B, 3, n)
         """
         shape = shape.unsqueeze(-1) # (B, K, 1, 1)
-
+        # averaging model keypoints
         return torch.einsum('bkmn,ukij->bij', shape, self.model_keypoints), torch.einsum('bkmn,ukij->bij', shape, self.cad_models)
 
 
